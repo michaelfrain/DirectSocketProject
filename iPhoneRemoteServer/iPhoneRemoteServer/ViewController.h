4bc1b031867986ff89c81b03b4954947074201f4
Michael Frain <michael.frain@hudl.com>
2015-03-09 09:07:44 -0700

    New project

---
+++ iPhoneRemoteServer/iPhoneRemoteServer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iPhoneRemoteServer
+//
+//  Created by Michael Frain on 3/9/15.
+//  Copyright (c) 2015 Michael Frain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
